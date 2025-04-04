52ff2578ad46421e81f7abac2cc394560e4a5b6a
MathiMat512 <mathias.mathimat12@gmail.com>
2025-04-04 09:05:53 -0500

    Commit en java

--- MiRepositorio/src/main/java/com/mycompany/mirepositorio/MiRepositorio.java
+++ MiRepositorio/src/main/java/com/mycompany/mirepositorio/MiRepositorio.java
@@ -1,8 +1,9 @@
 package com.mycompany.mirepositorio;
 
 public class MiRepositorio {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        for(var i=1; i<=100; i++){
+            System.out.println("El cuadrado de "+i+" es: "+i*i);
+        }
     }
 }
